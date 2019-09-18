cadd2fa016f4a1725c38ff4919b983bca6ae3852
Ramiz Khan <ramiz.cop@gmail.com>
2019-09-18 23:22:38 +0530

    Removed useless name

--- src/models/user.js
+++ src/models/user.js
@@ -4,11 +4,6 @@ const bcrypt = require('bcryptjs')
 const jwt = require('jsonwebtoken')
 
 const userSchema = new mongoose.Schema({
-    name: {
-        type: String,
-        required: true,
-        trim: true
-    },
     email: {
         type: String,
         unique: true,
