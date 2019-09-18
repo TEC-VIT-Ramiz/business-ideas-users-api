f316e1fba4c214dfe2bc4834e8bed073409a419a
Ramiz Khan <ramiz.cop@gmail.com>
2019-09-18 23:57:44 +0530

    Added cors

--- src/app.js
+++ src/app.js
@@ -1,10 +1,12 @@
 const express = require('express')
 require('./db/mongoose')
 const userRouter = require('./routers/user')
+const cors = require('cors')
 
 const app = express()
 
 app.use(express.json())
 app.use(userRouter)
+app.use(cors())
 
 module.exports = app
